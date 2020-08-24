43e331929827576bbfc702e4cb14c09115c9a313
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-08-25 00:18:45 +0700

    add: image validation, system cache image, refresh button

--- app/src/main/java/com/suncode/yesorno/MainActivity.java
+++ app/src/main/java/com/suncode/yesorno/MainActivity.java
@@ -7,13 +7,16 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private TextView textView;
     private ProgressBar progressBar;
+    private ImageButton refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,30 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView_result);
         progressBar = findViewById(R.id.progressbar);
+        refreshButton = findViewById(R.id.refresh);
+
+        //hide action bar
+        getSupportActionBar().hide();
+
+        //transparent status/notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        //change color text in status/notification bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         getData();
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+                refreshButton.setVisibility(View.GONE);
+
+                getData();
+            }
+        });
     }
 
     private void getData() {
@@ -48,11 +74,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Model> call, Response<Model> response) {
                 Model model = response.body();
 
-
-                progressBar.setVisibility(View.GONE);
-                imageView.setVisibility(View.VISIBLE);
-                textView.setVisibility(View.VISIBLE);
-                
+                //show image after drawable was loaded
                 Glide.with(getApplicationContext())
                         .load(model.getImage())
                         .listener(new RequestListener<Drawable>() {
@@ -61,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                                 progressBar.setVisibility(View.GONE);
                                 imageView.setVisibility(View.VISIBLE);
                                 textView.setVisibility(View.VISIBLE);
+                                refreshButton.setVisibility(View.VISIBLE);
                                 return false;
                             }
 
@@ -69,9 +92,11 @@ public class MainActivity extends AppCompatActivity {
                                 progressBar.setVisibility(View.GONE);
                                 imageView.setVisibility(View.VISIBLE);
                                 textView.setVisibility(View.VISIBLE);
+                                refreshButton.setVisibility(View.VISIBLE);
                                 return false;
                             }
                         })
+                        .diskCacheStrategy(DiskCacheStrategy.DATA)
                         .into(imageView);
                 
                 textView.setText(model.getAnswer());
